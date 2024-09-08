d7b516ba8ed382d33ff26eec682722d5b18e8e67
ChzuoL <chenzuoliu@gmail.com>
2024-09-07 21:46:40 -0700

    other html field types

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -174,6 +174,30 @@ export default function Lab1() {
                 <input type="text" id="wd-text-fields-last-name" placeholder="Liu"
                 value="Liu" title="The last name" />
                 {/* copy rest of form elements here  */}
+                <h4>Other HTML field types</h4>
+
+                <label htmlFor="wd-text-fields-email"> Email: </label>
+                <input type="email"
+                      placeholder="jdoe@somewhere.com"
+                      id="wd-text-fields-email"/><br/>
+
+                <label htmlFor="wd-text-fields-salary-start"> Starting salary:
+                </label>
+                <input type="number"
+                      id="wd-text-fields-salary-start"
+                      placeholder="1000"
+                      value="100000"/><br/>
+
+                <label htmlFor="wd-text-fields-rating"> Rating: </label>
+                <input type="range" id="wd-text-fields-rating"
+                      placeholder="Doe"
+                      max="5"
+                      value="4"/><br/>
+
+                <label htmlFor="wd-text-fields-dob"> Date of birth: </label>
+                <input type="date"
+                      id="wd-text-fields-dob"
+                      value="2000-01-21"/><br/>
           </form>
         </div>
         <h5>Text boxes</h5>
@@ -181,20 +205,17 @@ export default function Lab1() {
         <textarea id="wd-textarea" cols={30} rows={10}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
         
         {/* Buttons */}
-
         <h5 id="wd-buttons">Buttons</h5>
         <button id="wd-all-good" onClick={() => alert("Life is Good!")} type="button">
           Hello World!
         </button>
 
         {/* File upload button */}
-
         <h5>File upload</h5>
         <input id="wd-upload" type="file"/>
         
 
         {/* Radio buttons */}
-
         <h5 id="wd-radio-buttons">Radio buttons</h5>
 
         <label>Favorite movie genre:</label><br />
@@ -212,7 +233,6 @@ export default function Lab1() {
         <label htmlFor="wd-radio-fantasy">Fantasy</label>
 
         {/* CheckBoxes */}
-
         <h5 id="wd-checkboxes">Checkboxes</h5>
         <label>Favorite movie genre:</label><br/>
 
@@ -229,8 +249,28 @@ export default function Lab1() {
         <label htmlFor="wd-chkbox-fantasy">Fantasy</label>
 
         {/* Dropdowns */}
+        <h4 id="wd-dropdowns">Dropdowns</h4>
 
-        
+        <h5>Select one</h5>
+        <label htmlFor="wd-select-one-genre"> Favorite movie genre: </label><br/>
+        <select id="wd-select-one-genre">
+          <option value="COMEDY">Comedy</option>
+          <option value="DRAMA">Drama</option>
+          <option selected value="SCIFI">
+              Science Fiction</option>
+          <option value="FANTASY">Fantasy</option>
+        </select>
+
+        <h5>Select many</h5>
+        <label htmlFor="wd-select-many-genre"> Favorite movie genres: </label><br/>
+        <select id="wd-select-many-genre" multiple>
+          <option selected value="COMEDY">Comedy</option>
+          <option value="DRAMA">Drama</option>
+          <option selected value="SCIFI">
+              Science Fiction</option>
+          <option value="FANTASY">Fantasy</option>
+        </select>
+
       </div>
     );
   }
